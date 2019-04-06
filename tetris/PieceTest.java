4905d4f3aff5faeeb9af950df01c70d681670578
lkito <lkiti17@freeuni.edu.ge>
2019-04-06 19:56:14 +0400

    added some more piece tests. Piece likely done

--- tetris/PieceTest.java
+++ tetris/PieceTest.java
@@ -36,7 +36,7 @@ public class PieceTest {
 	// Here are some sample tests to get you started
 	
 	@Test
-	public void testSampleSize() {
+	public void testSize() {
 		// Check size of pyr piece
 		assertEquals(3, pyr1.getWidth());
 		assertEquals(2, pyr1.getHeight());
@@ -50,12 +50,54 @@ public class PieceTest {
 		Piece l = new Piece(Piece.STICK_STR);
 		assertEquals(1, l.getWidth());
 		assertEquals(4, l.getHeight());
+
+		Piece s = new Piece(Piece.S1_STR);
+        assertEquals(3, s.getWidth());
+        assertEquals(2, s.getHeight());
+
+        s = s.computeNextRotation();
+        assertEquals(2, s.getWidth());
+        assertEquals(3, s.getHeight());
+
 	}
 
 
+    @Test
+    public void testCustomSize() {
+        //Test non-existing pieces
+        s = new Piece("0 0  1 1  2 2  3 3");
+        assertEquals(4, s.getWidth());
+        assertEquals(4, s.getHeight());
+
+        s = s.computeNextRotation();
+        assertEquals(4, s.getWidth());
+        assertEquals(4, s.getHeight());
+
+        //Test non-existing "wrong" pieces
+        s = new Piece("1 1  2 1  3 1  4 1");
+        assertEquals(5, s.getWidth());
+        assertEquals(2, s.getHeight());
+
+        //computes "wrong" width, because the piece is not placed correctly
+        s = s.computeNextRotation();
+        assertEquals(1, s.getWidth());
+        assertEquals(5, s.getHeight());
+
+
+        //piece with a form of   ``|   (rotated L)
+        s = new Piece("0 3  1 3  2 3  3 3  4 3  4 2  4 1  4 0");
+        assertEquals(5, s.getWidth());
+        assertEquals(4, s.getHeight());
+
+        s = s.computeNextRotation();
+        assertEquals(4, s.getWidth());
+        assertEquals(5, s.getHeight());
+
+    }
+
 	// Test the skirt returned by a few pieces
 	@Test
-	public void testSampleSkirt() {
+	public void testSkirt() {
 		// Note must use assertTrue(Arrays.equals(... as plain .equals does not work
 		// right for arrays.
 		assertTrue(Arrays.equals(new int[] {0, 0, 0}, pyr1.getSkirt()));
@@ -63,16 +105,39 @@ public class PieceTest {
 
 		assertTrue(Arrays.equals(new int[] {0, 0, 1}, s.getSkirt()));
 		assertTrue(Arrays.equals(new int[] {1, 0}, sRotated.getSkirt()));
+
+        //Test non-existing pieces
+        Piece s = new Piece("0 0  1 1  2 2  3 3");
+        assertTrue(Arrays.equals(new int[] {0, 1, 2, 3}, s.getSkirt()));
+        s = s.computeNextRotation();
+        assertTrue(Arrays.equals(new int[] {3, 2, 1, 0}, s.getSkirt()));
+        s = s.computeNextRotation();
+        assertTrue(Arrays.equals(new int[] {0, 1, 2, 3}, s.getSkirt()));
+
+
+        //piece with a form of   ``|   (rotated L)
+        s = new Piece("0 3  1 3  2 3  3 3  4 3  4 2  4 1  4 0");
+        assertTrue(Arrays.equals(new int[] {3, 3, 3, 3, 0}, s.getSkirt()));
+        s = s.computeNextRotation();
+        assertTrue(Arrays.equals(new int[] {0, 4, 4, 4}, s.getSkirt()));
 	}
 
 
 	// Test the rotation methods
 	@Test
-	public void testSampleRotation() {
-		assertTrue(Piece.getPieces()[Piece.SQUARE].equals(Piece.getPieces()[Piece.SQUARE].fastRotation()));
-		assertFalse(Piece.getPieces()[Piece.STICK].equals(Piece.getPieces()[Piece.STICK].fastRotation()));
-		assertTrue(Piece.getPieces()[Piece.STICK].equals(Piece.getPieces()[Piece.STICK].fastRotation().fastRotation()));
-		assertTrue(Piece.getPieces()[Piece.S1].computeNextRotation().equals(Piece.getPieces()[Piece.S1].fastRotation()));
+	public void testRotationAndEquals() {
+		assertTrue(Piece.getPieces()[Piece.SQUARE]
+                .equals(Piece.getPieces()[Piece.SQUARE].fastRotation()));
+		assertFalse(Piece.getPieces()[Piece.STICK]
+                .equals(Piece.getPieces()[Piece.STICK].fastRotation()));
+		assertTrue(Piece.getPieces()[Piece.STICK]
+                .equals(Piece.getPieces()[Piece.STICK].fastRotation().fastRotation()));
+        assertTrue(Piece.getPieces()[Piece.S1].computeNextRotation()
+                .equals(Piece.getPieces()[Piece.S1].fastRotation()));
+        assertTrue(Piece.getPieces()[Piece.L2].computeNextRotation().computeNextRotation()
+                .equals(Piece.getPieces()[Piece.L2].fastRotation().fastRotation()));
+        assertFalse(Piece.getPieces()[Piece.L2].computeNextRotation().computeNextRotation().computeNextRotation()
+                .equals(Piece.getPieces()[Piece.L2].fastRotation().fastRotation()));
 	}
 	
 	
